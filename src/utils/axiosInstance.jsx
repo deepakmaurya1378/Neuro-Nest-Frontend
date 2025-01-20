410ac8240446baeb478d1da59dea4d35ea185633
Deepak Maurya <deepakmaurya1378@gmail.com>
2025-01-21 00:59:11 +0530

    change2

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://neuro-nest-api.onrender.com/', 
-  https://neuro-nest-api.onrender.com/
   timeout: 10000, // Optional: set the request timeout(in milliseconds)
   headers: {
     'Content-Type': 'application/json', // Default content type
